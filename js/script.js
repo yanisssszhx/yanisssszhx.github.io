487ff0d7c271e2fd70fcf6cfbc4fc1ead0b8600b
yanisssszhx <yaniss.bouhamdi@icloud.com>
2026-01-14 04:00:32 +0100

    Delete js directory

--- js/script.js
+++
@@ -1,6 +0,0 @@
-const form = document.querySelector("form");
-form.addEventListener("submit", function(e){
-   e.preventDefault();
-   alert("Merci pour votre message ! Nous vous répondrons bientôt.");
-   form.reset();
-});
